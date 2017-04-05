01d219d1c1678c6628f3262a00a5b1accbbdd7ab
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 14:17:23 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-loader-html
-Webpack Loader for Vue/HTML
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Loader HTML
+====================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-loader-html.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-loader-html.png" alt=""/>
+
+About
+-----
+
+This is the Webpack Loader for HTML files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+It is primarily based on [PostHTML](https://github.com/posthtml/posthtml) and [VueJS](https://vuejs.org/).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-loader-html.js
@@ -0,0 +1,92 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const co               = require("co")
+const loaderUtils      = require("loader-utils")
+const PostHTML         = require("posthtml")
+const PostHTMLBlock    = require("posthtml-block")
+const PostHTMLScope    = require("style-scope/posthtml")
+const PostHTMLMarkdown = require("posthtml-md")
+const PostHTMLLorem    = require("posthtml-lorem")
+const inlineAssets     = require("inline-assets")
+const vueValidator     = require("vue-template-validator")
+const vueCompiler      = require("vue-template-compiler")
+const jsBeautify       = require("js-beautify").js_beautify
+
+/*  the exported Webpack loader function  */
+module.exports = function (content) {
+    co(function * () {
+        const done = this.async()
+
+        /*  determine Webpack loader query parameters  */
+        const options = Object.assign({}, {
+            scope: "none"
+        }, loaderUtils.getOptions(this), this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : null)
+
+        /*  indicate to Webpack that our results are
+            fully deterministic and can be cached  */
+        this.cacheable(true)
+
+        /*  process HTML markup  */
+        let response = yield (PostHTML([
+            PostHTMLBlock,
+            PostHTMLScope({ rootScope: options.scope }),
+            PostHTMLMarkdown(),
+            PostHTMLLorem()
+        ]).process(content, {
+            closingSingleTag: "default"
+        }))
+        let result = response.html
+
+        /*  inline all referenced assets to be self-contained  */
+        result = inlineAssets(this.resourcePath, this.resourcePath, result, {
+            htmlmin: this.minimize,
+            cssmin:  this.minimize,
+            jsmin:   false,
+            pattern: [ ".+" ],
+            purge:   false
+        })
+
+        /*  remove leading and trailing comments as
+            Vue expects a single top-level DOM element  */
+        result = result
+            .replace(/^(?:.|\n)*?(<[a-zA-Z_])/, "$1")
+            .replace(/(?:[^>]|\n)*$/, "")
+
+        /*  validate HTML template for Vue  */
+        var warnings = vueValidator(result)
+        if (warnings.length > 0)
+            this.emitWarning("gemstone-webpack-loader-html: Vue [template-validator]: " +
+                `WARNING:\n${warnings.join("\n")}`)
+
+        /*  compile HTML template into a Vue rendering object  */
+        let renderer = vueCompiler.compile(result)
+        if (renderer.errors && renderer.errors.length > 0) {
+            this.emitError("gemstone-webpack-loader-html: Vue [template-compiler]: " +
+                `ERROR: ${renderer.errors.join("\n")}`)
+            renderer = {
+                render: "throw new Error(\"Vue template compilation already failed under build-time\")",
+                staticRenderFns: []
+            }
+        }
+
+        /*  export Vue rendering object as a JavaScript string  */
+        const toFunction = (code) =>
+            "function () { " + jsBeautify(code, { indent_size: 4 }) + " }"
+        result = `module.exports = {
+            render: ${toFunction(renderer.render)},
+            staticRenderFns: [
+                ${renderer.staticRenderFns.map(toFunction).join(",\n")}
+            ]
+        }`
+
+        done(null, result)
+    }.bind(this)).catch((err) => {
+        this.emitError("gemstone-webpack-loader-html: ERROR: " + err)
+    })
+}
+
